7b7d01f65acbe7c19e0087a60db4b53630fa1f29
Aryan Yadav <yadavaryan9661@gmail.com>
2025-08-01 10:08:10 +0530

    Clean Habit Tracker for Codemagic

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -5,9 +5,10 @@ plugins {
     id("dev.flutter.flutter-gradle-plugin")
 }
 
-android {
-    namespace = "com.example.habitt_tracker"
-    compileSdk = flutter.compileSdkVersion
+android {ndkVersion = "27.0.12077973"}
+
+    namespace = "com.aryan.habitt_tracker"
+    compileSdk = 34
     ndkVersion = flutter.ndkVersion
 
     compileOptions {
@@ -21,13 +22,13 @@ android {
 
     defaultConfig {
         // TODO: Specify your own unique Application ID (https://developer.android.com/studio/build/application-id.html).
-        applicationId = "com.example.habitt_tracker"
+        applicationId = "com.aryan.habitt_tracker"
         // You can update the following values to match your application needs.
         // For more information, see: https://flutter.dev/to/review-gradle-config.
-        minSdk = flutter.minSdkVersion
-        targetSdk = flutter.targetSdkVersion
-        versionCode = flutter.versionCode
-        versionName = flutter.versionName
+        minSdk = 23
+        targetSdk = 34
+        versionCode = 1
+        versionName = "1.0"
     }
 
     buildTypes {
